4d0545121fdf7214df61e75168186473b3d1e09e
Alam <rashadul91@gmail.com>
2018-08-05 20:10:55 +0600

    login structure complete

--- admin/login.php
+++ admin/login.php
@@ -7,20 +7,50 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="../apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" href="../css/main.css">
+        <script src="../js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+
+        <div class="container">
+            <div class="row">
+                <div class="login-outer">
+                    <div class="login-inner">
+                        <div class="col-md-4 col-md-offset-4">
+                        
+                            <form action="" method="POST">
+                                    <h1>System login</h1>
+                                <div class="form-group">
+                                    <label for="username">Username: </label>
+                                    <input type="text" id="username" class="form-control" name="username">
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password: </label>
+                                    <input type="text" name="password" id="password" class="form-control">
+                                </div>
+                                <input type="submit" name="form1" value="Sign in" class="btn btn-default">
+                            </form>
+
+                        </div>
+                    </div><!-- .login-inner -->
+                </div><!-- login-outer -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+
+
+
+
+
+
 
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
